898a92a8ce5f71a7d670575d94f57a58a4c788d1
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2023-09-19 15:14:24 +0700

    update: refactor flower func: render, setstate

--- 05-SceneManager/GreenFlower1.cpp
+++ 05-SceneManager/GreenFlower1.cpp
@@ -4,29 +4,21 @@
 #include "debug.h"
 CGreenFlower1::CGreenFlower1(float x, float y) :CGameObject(x, y)
 {
-	this->ax = 0;
-	this->ay = 0;
-	SetState(FLOWER_STATE_ACTIVE);
-	yLimit = y - FLOWER_BBOX_HEIGHT;
+	ay = 0;
+	top = y - FLOWER_BBOX_HEIGHT;
+	bot = y;
 	loop_start = -1;
+	is_after_shooting = false;
+
+	SetState(FLOWER_STATE_STATIC);
 }
 
 void CGreenFlower1::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 {
-	if (state == FLOWER_STATE_ONBOTTOM)
-	{
-		left = 0;
-		top = 0;
-		right = 0;
-		bottom = 0;
-	}
-	else {
-
-		left = x - FLOWER_BBOX_WIDTH / 2;
-		top = y - (FLOWER_BBOX_HEIGHT / 2);
-		right = left + FLOWER_BBOX_WIDTH;
-		bottom = top + FLOWER_BBOX_HEIGHT;
-	}
+	left = x - FLOWER_BBOX_WIDTH / 2;
+	top = y - (FLOWER_BBOX_HEIGHT / 2);
+	right = left + FLOWER_BBOX_WIDTH;
+	bottom = top + FLOWER_BBOX_HEIGHT;
 }
 
 void CGreenFlower1::OnNoCollision(DWORD dt)
@@ -40,40 +32,48 @@ void CGreenFlower1::OnCollisionWith(LPCOLLISIONEVENT e)
 
 void CGreenFlower1::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	//DebugOut(L">>> Count time >>> %d \n", GetTickCount64() - loop_start);
 	float px, py;
 	CGame::GetInstance()->GetCurrentScene()->getPlayerPosition(px, py);
 
-	if ((state == FLOWER_STATE_ONTOP) && (GetTickCount64() - loop_start > FLOWER_LOOP_TIMEOUT))
-	{
-		SetState(FLOWER_STATE_POW);
-		return;
-	}
-	if ((state == FLOWER_STATE_POW) && (GetTickCount64() - loop_start > FLOWER_LOOP_TIMEOUT))
-	{
+	vy += ay * dt;
 
-		CGame::GetInstance()->GetCurrentScene()->createNewObject(OBJECT_TYPE_FIRE, x, y);
-		SetState(FLOWER_STATE_ACTIVE);
-		return;
-	}
-	if ((state == FLOWER_STATE_ONBOTTOM) && (GetTickCount64() - loop_start > FLOWER_BOTTOM_LOOP_TIMEOUT))
+	x += vx * dt;
+	y += vy * dt;
+
+	if (y < top)
 	{
-		if (px >= x - 50 && px < x + 50) return;
-		SetState(FLOWER_STATE_ACTIVE);
-		return;
+		y = top;
+		SetState(FLOWER_STATE_STATIC);
+	}
+	else if (y > bot)
+	{
+		y= bot;
+		SetState(FLOWER_STATE_STATIC);
+	
 	}
 
-	if (y < yLimit)
+	if (state==FLOWER_STATE_STATIC)
 	{
-		SetState(FLOWER_STATE_ONTOP);
-	}
-	else if (y > yLimit + FLOWER_BBOX_HEIGHT)
-	{
-		SetState(FLOWER_STATE_ONBOTTOM);
+		if (y == top)
+		{
+			if (is_after_shooting && (GetTickCount64() - loop_start > FLOWER_AFSHOOT_TIMEOUT)) {
+				is_after_shooting = false;
+				SetState(FLOWER_STATE_MOVE);
+			}
+			else if(GetTickCount64() - loop_start > FLOWER_PRESHOOT_TIMEOUT)
+			{
+				is_after_shooting = true;
+				loop_start = GetTickCount64();
+				shooting();
+			}
+		}
+		else if (y == bot && (GetTickCount64() - loop_start > FLOWER_LOOP_TIMEOUT))
+		{
+			SetState(FLOWER_STATE_MOVE);
+		}
 	}
 
 	CGameObject::Update(dt, coObjects);
-	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
 int CGreenFlower1::getMovingFlowerAniId(int flag) {
 	switch (flag)
@@ -119,7 +119,7 @@ void CGreenFlower1::Render()
 {
 	int flag = getFlowerPosition();
 	int aniId;
-	if (this->state == FLOWER_STATE_ONTOP) {
+	if (this->state == FLOWER_STATE_STATIC) {
 		aniId = getStaticFlowerAniId(flag);
 	}
 	else {
@@ -133,27 +133,19 @@ void CGreenFlower1::SetState(int state)
 	CGameObject::SetState(state);
 	switch (state)
 	{
-	case FLOWER_STATE_POW:
-		break;
-	case FLOWER_STATE_ONTOP:
-		y = yLimit;
+	case FLOWER_STATE_STATIC:
 		loop_start = GetTickCount64();
+		ay = 0;
 		vy = 0;
 		break;
-	case FLOWER_STATE_ONBOTTOM:
-		y = yLimit + FLOWER_BBOX_HEIGHT;
-		loop_start = GetTickCount64();
-		vy = 0;
-		break;
-	case FLOWER_STATE_ACTIVE:
-		if (y <= yLimit)
-		{
-			vy = FLOWER_SPEED;
-		}
-		else if (y >= yLimit + FLOWER_BBOX_HEIGHT)
-		{
-			vy = -FLOWER_SPEED;
-		}
+	case FLOWER_STATE_MOVE:
+		if (y == top) ay = FLOWER_AXIS_Y;
+		else if (y==bot) ay = -FLOWER_AXIS_Y;
 		break;
 	}
+}
+void CGreenFlower1::shooting() {
+	float px, py;
+	CGame::GetInstance()->GetCurrentScene()->getPlayerPosition(px, py);
+	CGame::GetInstance()->GetCurrentScene()->createNewObject(OBJECT_TYPE_FIRE, x, y);
 }
--- 05-SceneManager/GreenFlower1.h
+++ 05-SceneManager/GreenFlower1.h
@@ -1,18 +1,17 @@
 #pragma once
 #include "GameObject.h"
 
-#define FLOWER_SPEED 0.03
+#define FLOWER_AXIS_Y 0.0001f
+
 #define FLOWER_BBOX_WIDTH 16
 #define FLOWER_BBOX_HEIGHT 32
 
-#define FLOWER_LOOP_TIMEOUT 1500
-#define FLOWER_BOTTOM_LOOP_TIMEOUT 3000
+#define FLOWER_PRESHOOT_TIMEOUT 1500
+#define FLOWER_AFSHOOT_TIMEOUT 750
+#define FLOWER_LOOP_TIMEOUT 3000
 
-#define FLOWER_STATE_ACTIVE 100
-#define FLOWER_STATE_POW 200
-#define FLOWER_STATE_STATIC 300
-#define FLOWER_STATE_ONTOP 400
-#define FLOWER_STATE_ONBOTTOM 500
+#define FLOWER_STATE_MOVE 100
+#define FLOWER_STATE_STATIC 200
 
 #define ID_ANI_FLOWER_UP_LEFT_IDLE 19001
 #define ID_ANI_FLOWER_DOWN_LEFT_IDLE 19002
@@ -30,13 +29,21 @@ class CGreenFlower1 : public CGameObject
 {
 
 protected:
-	float yLimit;
-	float ax;
 	float ay;
+	float top;
+	float bot;
+	bool is_after_shooting;
 	float ani_pow_id;
 
 	ULONGLONG loop_start;
 
+	int getFlowerPosition();
+	int getMovingFlowerAniId(int flag);
+	int getStaticFlowerAniId(int flag);
+	void shooting();
+
+public:
+	CGreenFlower1(float x, float y);
 
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
@@ -46,12 +53,5 @@ protected:
 	virtual void OnNoCollision(DWORD dt);
 
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
-
-	int getFlowerPosition();
-	int getMovingFlowerAniId(int flag);
-	int getStaticFlowerAniId(int flag);
-
-public:
-	CGreenFlower1(float x, float y);
 	virtual void SetState(int state);
 };
