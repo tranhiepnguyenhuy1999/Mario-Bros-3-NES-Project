4539aa5df5807382055b36b9bc7ead696fc0d6c2
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2023-11-24 21:30:38 +0700

    update: bbox of down brick

--- 05-SceneManager/DownBrick.cpp
+++ 05-SceneManager/DownBrick.cpp
@@ -7,11 +7,7 @@ void CDownBrick::Render()
 
 void CDownBrick::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
-	l = x - BRICK_BBOX_WIDTH * length / 2;
-	if (length % 2 == 0)
-	{
-	l = x - BRICK_BBOX_WIDTH*length / 2 - 8;
-	}
+	l = x - BRICK_BBOX_WIDTH / 2;
 	t = y - BRICK_BBOX_HEIGHT / 2;
 	r = l + length * BRICK_BBOX_WIDTH;
 	b = t + BRICK_BBOX_HEIGHT;
